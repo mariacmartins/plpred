5dd992073d44ccfa4989e03fb2e27eeeaa313a69
mariacmartins <maria.c.martins07@gmail.com>
2021-06-25 17:03:25 -0300

    async execution with celery

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: python plpred/server.py --port $PORT --host 0.0.0.0 --model data/models/model.pickle
+worker: celery -A plpred.worker.celery worker
--- plpred/server.py
+++ plpred/server.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
-from plpred.preprocessing import compute_aa_composition
+from flask import Flask, render_template, request, redirect
+from celery.result import AsyncResult
+from plpred.worker import celery, predict_protein_location
 import pandas as pd
-import pickle
 import argparse
 
 app = Flask(__name__)
@@ -13,28 +13,20 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     protein_sequence = request.form.get('protein_sequence')
-    aa_composition = compute_aa_composition(protein_sequence)
-    df_aa_composition = pd.DataFrame([aa_composition])
-    prediction = app.config['model'].predict(df_aa_composition)
+    job_id = predict_protein_location.apply_async((protein_sequence,))
+    return redirect(f'/results/{job_id}')
 
-    if prediction == 1:
-        protein_location = 'membrane'
-    else:
-        protein_location = 'cytoplasm'
-
-    return render_template('index.html', protein_location=protein_location)
+@app.route('/results/<job_id>', methods=['GET'])
+def results(job_id:str):
+    job_result = AsyncResult(job_id, app=celery)
+    return render_template('results.html', job_result=job_result)
 
 def main():
     argument_parser = argparse.ArgumentParser(description='plpred-server: subcellular location prediction server')
     argument_parser.add_argument('-H', '--host', required=True, help='host adress')
     argument_parser.add_argument('-p', '--port', required=True, help='host port')
-    argument_parser.add_argument('-m', '--model', required=True, help='trained model to be deployed')
     arguments = argument_parser.parse_args()
         
-    with open(arguments.model, 'rb') as handle:
-        app.config['model'] = pickle.load(handle)
-
-
     app.run(host=arguments.host, port=arguments.port)
 
 if __name__ == '__main__':
